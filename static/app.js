f449f081fdc64822430ba2b98bf5940cbb73e78c
Antonio Salvati <antonio.salvati@skillbill.it>
2020-04-10 16:59:48 +0200

    scroll fix

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@
 
   function loadPage(page) {
     main.innerHTML = '<div class="spinner"></div>';
-    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollTop = 50;
 
     return fetch(`partials/${page.file}.html`)
       .then(res => res.text())
@@ -78,9 +78,9 @@
 (function scroll() {
   document.body.classList.add('on-top');
   window.addEventListener('scroll', () => {
-    if(document.documentElement.scrollTop === 0 && !document.body.classList.contains('on-top')) {
+    if(document.documentElement.scrollTop <= 40 && !document.body.classList.contains('on-top')) {
       document.body.classList.add('on-top');
-    } else if(document.body.classList.contains('on-top')) {
+    } else if(document.documentElement.scrollTop > 40 && document.body.classList.contains('on-top')) {
       document.body.classList.remove('on-top');
     }
   });
