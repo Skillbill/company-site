03d628891908f15c569430e809827cf3ea21f5a0
Antonio Vivone <ndonio96@gmail.com>
2021-02-04 17:16:08 +0100

    logo to /

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const buttonNavBar = document.querySelector("button.navbar-toggler")
 buttonNavBar.addEventListener("click", () => {
   const expanded = buttonNavBar.getAttribute("aria-expanded")
   const navBarMobile = document.querySelector(".navbar-collapse")
-  
+
   if (expanded === "false") {
     buttonNavBar.setAttribute("aria-expanded", true)
 
@@ -31,18 +31,18 @@ var activeNavLinks = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       const sectionId = entry.target.id;
-      
+
       Array.from(document.querySelectorAll(".navbar a")).forEach(a => {
         a.classList.remove("active")
       })
 
       const sectionLink = document.querySelector(`a[href="#${sectionId}"]`);
-      if(sectionLink) {
+      if (sectionLink) {
         sectionLink.classList.add("active");
       }
     }
   })
-}, { rootMargin: "-30% 0%"})
+}, { rootMargin: "-30% 0%" })
 
 Array.from(document.querySelectorAll("section")).forEach(section => {
   activeNavLinks.observe(section)
@@ -58,26 +58,29 @@ const shufflePeople = function people() {
     return a;
   }
 
-  const profiles = document.querySelector('.profiles'); 
+  const profiles = document.querySelector('.profiles');
   const people = document.querySelectorAll('.profiles > div');
   shuffle(new Array(people.length).fill(0).map((_, i) => i)).forEach(index => {
     profiles.appendChild(people[index]);
   });
 }
 
-shufflePeople();  
+shufflePeople();
 
 var navbar = document.querySelector("nav")
 
+var logo_home = document.querySelector(".navbar-brand-home")
+
 var navObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting) {
+    if (entry.isIntersecting) {
       if (!navbar.classList.contains("absolute")) {
         navbar.classList.remove("fixed-top")
         navbar.classList.remove("shadow-sm")
         navbar.classList.remove("bg-light")
         navbar.classList.remove("fade-in")
         navbar.classList.add("absolute")
+        logo_home.classList.remove("hidden")
       }
     } else {
       navbar.classList.add("fade-in")
@@ -85,8 +88,9 @@ var navObserver = new IntersectionObserver(entries => {
       navbar.classList.add("shadow-sm")
       navbar.classList.add("bg-light")
       navbar.classList.remove("absolute")
+      logo_home.classList.add("hidden")
     }
   })
-}, { rootMargin: "-40% 0%"})
+}, { rootMargin: "-40% 0%" })
 
 navObserver.observe(document.querySelector("header"))
